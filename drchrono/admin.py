769d43fddc973c1d80a61466b25c854502bcfd9c
David Ford <david@blue-labs.org>
2018-05-15 00:36:40 -0400

    add missing import

--- drchrono/admin.py
+++ drchrono/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Office, Doctor, Patient
+from .models import Office, Doctor, Patient, Appointment
 
 admin.site.register(Office)
 admin.site.register(Doctor)
